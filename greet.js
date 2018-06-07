d3e255eea8212386b2127b56844af785fe688279
Londiwe <celelondiwe3@gmail.com>
2018-06-07 14:47:48 +0200

    done with the Dom part of the exercise

--- greet.js
+++ greet.js
@@ -1,67 +1,55 @@
 var NameElement = document.querySelector('.Name');
-  var greetBtnElement = document.querySelector('.greetBtn');
-  var greetingElement = document.querySelector('.greeting');
-  var greetingCounterElement = document.querySelector('.greetCounter');
+var greetBtnElement = document.querySelector('.greetBtn');
+var greetingElement = document.querySelector('.greeting');
+var greetingCounterElement = document.querySelector('.greetCounter');
+var resetElement = document.querySelector('.reset');
 
-  var greeting = "";
-  var people = {};
+var greeting = "";
+var people = {};
 
+var clickTheGreetButton = function() {
 
-   var clickTheGreetButton = function(){
-
-
-          var checkedRadioBtn = document.querySelector("input[name='Language']:checked");
-    if (checkedRadioBtn){
-        var language = checkedRadioBtn.value;
-
-    }
-       var Name = NameElement.value;
-       if(Name != ''){
-
-
-      if(people[Name] === undefined){
-
-      people[Name] = 0;
-        }
+  var checkedRadioBtn = document.querySelector("input[name='Language']:checked");
+  if (checkedRadioBtn) {
+    var language = checkedRadioBtn.value;
 
   }
-        if (language === 'English') {
-          greeting = "Hello, " + Name;
-        }
-        if(language === 'Chinese'){
-          greeting = "你好(Nǐ hǎo) " + Name;
-        }
-        if (language === 'Venda') {
-          greeting = "Ndaa " + Name }
-          console.log(people);
+  var Name = (NameElement.value).toUpperCase();
+  if (Name != '') {
 
 
+    if (people[Name] === undefined) {
 
+      people[Name] = 0;
+    }
 
+  }
+  if (language === 'English') {
+    greeting = "Hello " + Name;
+  }
+  if (language === 'Chinese') {
+    greeting = "你好(Nǐ hǎo) " + Name;
+  }
+  if (language === 'Venda') {
+    greeting = "Ndaa " + Name
+  }
+  console.log(people);
 
-          greetingCounterElement.innerHTML = Object.entries(people).length ;
+  greetingCounterElement.innerHTML = Object.entries(people).length;
 
-          greetingElement.innerHTML = greeting;
-          NameElement.value = '';
-          // greetingCounterElement.innerHTML = Object.keys(people).length - 1;
+  greetingElement.innerHTML = greeting;
+  NameElement.value = '';
+}
 
+greetBtnElement.addEventListener('click', clickTheGreetButton);
 
+var reset = function() {
+  localStorage.removeItem('people');
+  localStorage.clear();
+  location.reload()
+}
 
+resetElement.addEventListener('click', reset);
 
-
-
-        }
-
-  // function greet(name){
-  //   if (name.length > 2){
-  //       return "Hello " +  name;
-  //   }
-  // }
-
-  greetBtnElement.addEventListener('click', clickTheGreetButton);
-
-
-
-
-  //console.log(firstNameElement);
-  //alert("yo!");
+//console.log(firstNameElement);
+//alert("yo!");
---
+++ greetest/greet-test.js
@@ -0,0 +1,14 @@
+describe("greet", function(){
+it("should greet Londiwe in English",function(){
+      var Hello = Greeting()
+      Hello.greetZ("Londiwe", 'English')
+      assert.equal(Hello.greetingZ())
+
+});
+
+
+
+
+
+
+});
